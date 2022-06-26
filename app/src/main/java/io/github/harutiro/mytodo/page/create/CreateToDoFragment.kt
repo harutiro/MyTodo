f53af46df9e29dec662a4584ae9d5b174a863a2e
harutiro <harutiro2027@outlook.jp>
2022-06-26 21:25:14 +0900

    update:ToDo作成画面完成 ※レイアウト部分

--- app/src/main/java/io/github/harutiro/mytodo/page/create/CreateToDoFragment.kt
+++ app/src/main/java/io/github/harutiro/mytodo/page/create/CreateToDoFragment.kt
@@ -1,12 +1,24 @@
 package io.github.harutiro.mytodo.page.create
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.harutiro.mytodo.MainViewModel
 import io.github.harutiro.mytodo.R
 
 @AndroidEntryPoint
 class CreateToDoFragment:Fragment(R.layout.create_todo_fragment) {
     private val vm:CreateToDoViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_create,menu)
+    }
 }
